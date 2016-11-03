d2a78cabe89e081ec5302cbee7b31d972f11658d
Aedan <nothingwillgowrong@gmail.com>
2016-11-02 19:51:10 -0600

    There is now a login form. Currently does not function.

--- squirespace/blog/views.py
+++ squirespace/blog/views.py
@@ -8,6 +8,10 @@ def user_registration(request):
     form = PostForm()
     return render(request, 'blog/user_signup.html', {'form': form})
 
+def login(request):
+    form = PostForm()
+    return render(request, 'blog/login.html', {'form': form})
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
