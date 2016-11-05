4ad8c0f515a0ddcd0cefe3448b8f58adc750d8cf
Rishi Barnwal <Batcomputer@uws108015usr.uws.ualberta.ca>
2016-11-04 19:40:47 -0600

    Friends almost works

--- squirespace/blog/urls.py
+++ squirespace/blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from views import UserRegPage
+# from views import UserRegPage
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
@@ -13,8 +13,8 @@ urlpatterns = [
 	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 	url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
 	url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
-	url(r'^reg/$', UserRegPage.as_view()),
-	url(r'^reg/confirm/$', views.user_registration, name = 'user_registration'),
+	url(r'^reg/$',views.register,name='register'),
+	url(r'^reg/confirm/$', views.registration_complete, name = 'registration_complete'),
 	url(r'^login/$', auth_views.login, name='login'),
   	url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
   	url(r'^friends/$', views.show_friends, name='friends'),
