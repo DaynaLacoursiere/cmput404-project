9502a6d0ba4e9d1f66245ad7ffb2ec93cb7061b6
DaynaLacoursiere <dlacours@ualberta.ca>
2016-11-16 14:31:45 -0700

    re #20: images are only shown if the user is authenticated... If the user is logged in but is not friends with the person, they can see the images though... not sure if thats an issue. It might be a better fix to make it so when you post images, you can only set the post to be 'private to my friends' or 'private to me'

--- squirespace/blog/views.py
+++ squirespace/blog/views.py
@@ -69,7 +69,7 @@ def image(request):
 """
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now())
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
