6ccd9d5d71305b8fb8f12c9317582e69e04a306b
dsopel <dsopel@ualberta.ca>
2016-11-17 03:23:36 -0700

    added to tests

--- squirespace/api/tests.py
+++ squirespace/api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import AbstractBaseUser, UserManager, User
 
 # Create your tests here.
+c = Client()
+
+class UserApiTest(TestCase):
+	def setUp(self):
+		self.user = User.objects.create_user(username = "John", email = "john@mailinator.com", password="a")
+		c.login(username='John', password='a')	
+	def testUserApiGet(self):
+		req = c.get('/api/users/1')
+		self.assertEqual(req.status_code, 200)
+		req = c.get('/api/users/2')
+		self.assertEqual(req.status_code, 404)
+
+	def testUserApiPost(self):
+		req = c.post('/api/users/', {"username":"xxz", "email":"xx@x.ca"})
+		self.assertEqual(req.status_code, 201) #201 is Created
--- squirespace/blog/tests.py
+++ squirespace/blog/tests.py
@@ -19,15 +19,20 @@ class postTest(TestCase):
 	def setUp(self):
 		self.user = User.objects.create_user(username = "John", email = "john@mailinator.com", password="a")
 		c.login(username='John', password='a')	
+		author = User.objects.get(username = "John")
+		self.tpost = Post.objects.create(author = author, title = "Test post please ignore", text = "yep")
+		self.tpost.save()
 
 # test methods must begin with test		
 	def testCreatePost(self):
 		author = User.objects.get(username = "John")
-		tpost = Post.objects.create(author = author, title = "Test post please ignore", text = "yep")
 		self.assertTrue(isinstance(author, User))
-		self.assertEqual(tpost.author, self.user)
-		self.assertEqual(tpost.text, "yep")
+		self.assertEqual(self.tpost.author, self.user)
+		self.assertEqual(self.tpost.text, "yep")
 
+	def testPrivacyLevel(self):
+		self.assertEqual(self.tpost.privatelevel, (u'public', u'Public'))
+	
 	def testPostViews(self):
 
 
@@ -43,3 +48,17 @@ class postTest(TestCase):
 
 		response = c.get('/')
 		self.assertEqual(response.status_code, 200)
+
+
+class CommentModelTests(TestCase):
+
+	def setUp(self):
+		self.user = User.objects.create_user(username = "John", email = "john@mailinator.com", password="a")
+		c.login(username='John', password='a')	
+		post = Post.objects.create(author = self.user, title = "Test post please ignore", text = "yep")
+		self.comment = Comment.objects.create(post = post, author = self.user, text = "commenttext")
+		self.comment.save()
+
+	def testCreateComment(self):
+		self.assertIsNotNone(self.comment.post)
+		self.assertEqual(self.comment.author, self.user)
