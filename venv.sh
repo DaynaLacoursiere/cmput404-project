aa2537f987435143d5a16c0356c78e1049c9fdf6
Aedan <nothingwillgowrong@gmail.com>
2016-11-18 14:39:47 -0700

    Added venv.sh this script installs and activates venv for you.

---
+++ venv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+virtualenv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install Pillow
+pip install Django==1.10
+pip install django-friendship
+pip install djangorestframework
