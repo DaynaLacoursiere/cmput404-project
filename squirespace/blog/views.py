bcafa2fa85a0678112bc9c77b38ba0c2efc431e0
Aedan <nothingwillgowrong@gmail.com>
2016-11-20 23:06:01 -0700

    There is now actual parsing being done for git events! It's absolute madness! Update your API you scumlords!

--- squirespace/blog/views.py
+++ squirespace/blog/views.py
@@ -14,15 +14,59 @@ from django.template.context_processors import csrf
 import requests
 
 def gitParse(payload):
-    eventType = payload.json()[0]['type']
+    i = 0
 
-    if (eventType == "PushEvent"):
-        return " pushed "+ str(payload.json()[0]['payload']['size']) + " commit(s) to "+ payload.json()[0]['repo']['name']+"."
-    if (eventType == "WatchEvent"):
-        following = payload.json()[0]['repo']['name']
-        return " starred the repository "+str(following)+"."
-    else:
-        return " performed an unspecified action."
+    # Loop should in theory, exit even if i goes to infinity.
+    # This is just a safety method until a "better" way to do this is found.
+    while(i < 100):
+
+        # This will only fail if we run out of events.
+        try:
+            eventType = payload.json()[i]['type']
+
+        # Out of valid events, just leave a generic message.
+        except:
+            return " checkout their Github for more details!"
+
+        # Try to find one of these events, if not move to the next.
+        else:
+            print(eventType)
+            if (eventType == "PushEvent"):
+                # This try is necessary for really dumb reasons. Update your API Github! You butts!
+                try:
+                    number = payload.json()[0]['payload']['size']
+                except:
+                    number = " a "
+                repo = payload.json()[0]['repo']['name']
+                return " pushed "+ str(number) + " commit(s) to "+ str(repo)+"."
+            elif (eventType == "WatchEvent"):
+                repo = payload.json()[0]['repo']['name']
+                return " starred the repository "+str(repo)+"."
+            elif (eventType == "CreateEvent"):
+                repo = payload.json()[0]['repo']['name']
+                return " created the repository "+str(repo)+"."
+            elif (eventType == "CommitCommentEvent"):
+                repo = payload.json()[0]['repo']['name']
+                comment = payload.json()[0]['payload']['comment']['body']
+                return ' commented "'+str(comment)+'" on a commit to '+str(repo)+"."
+            elif (eventType == "ForkEvent"):
+                repo = payload.json()[0]['repo']['name']
+                return " forked the repository "+str(repo)+"."
+            elif (eventType == "IssueCommentEvent"):
+                repo = payload.json()[0]['repo']['name']
+                comment = payload.json()[0]['payload']['comment']['body']
+                return ' commented "'+str(comment)+'" on an issue in the repository '+str(repo)+"."
+            elif (eventType == "IssuesEvent"):
+                action = payload.json()[0]['payload']['action']
+                title = payload.json()[0]['payload']['issue']['title']
+                repo = payload.json()[0]['repo']['name']
+                return " "+str(action)+' the "'+str(title)+'" issue on the repository '+str(repo)+"."
+            elif (eventType == "LabelEvent"):
+                action = payload.json()[0]['payload']['action']
+                repo = payload.json()[0]['repo']['name']
+                return " "+str(action)+' the label on the repository '+str(repo)+"."   
+            else:
+                i += 1
 
 def register(request):
     if request.method == 'POST':
