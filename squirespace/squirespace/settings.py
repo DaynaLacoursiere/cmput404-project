cc1a522ae4771aac41981fbc01720620be1037a4
preyansh <preyansh@ualberta.ca>
2016-11-21 09:16:30 -0700

    added some things for HTTPBasicAuth, trying to figure out how it actually works

--- squirespace/squirespace/settings.py
+++ squirespace/squirespace/settings.py
@@ -44,7 +44,11 @@ INSTALLED_APPS = [
 
 ]
 
-MIDDLEWARE_CLASSES = [
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.RemoteUserBackend',
+]
+
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +56,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
 ]
 
 ROOT_URLCONF = 'squirespace.urls'
