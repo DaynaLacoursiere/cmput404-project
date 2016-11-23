894d329e7561e752356e94ebbaaef780a858d948
Aedan <nothingwillgowrong@gmail.com>
2016-11-22 23:42:02 -0700

    Fixed.

--- squirespace/blog/views.py
+++ squirespace/blog/views.py
@@ -158,38 +158,43 @@ def post_list(request):
 
     # Socknet Posts
     headers = {'User-agent': 'SquireSpace'}
-    socknetjson = requests.get('http://cmput404f16t04dev.herokuapp.com/api/posts/', headers=headers, auth=('admin', 'cmput404'))
-    for i in socknetjson.json()['posts']:
-        sauthor = str(i['author']['displayName'])
-        stitle = str(i['title'])
-        stext = str(i['content'])
-        sid = i['id']
+    try:
+        socknetjson = requests.get('http://cmput404f16t04dev.herokuapp.com/api/posts/', headers=headers, auth=('admin', 'cmput404'))
+        assert(socknetjson.status_code == 200)
+    except:
+        print("Failed to grab SockNet API")
+    else:
+        for i in socknetjson.json()['posts']:
+            sauthor = str(i['author']['displayName'])
+            stitle = str(i['title'])
+            stext = str(i['content'])
+            sid = i['id']
 
-        # If it's a new SockNet poster, make a fake user on their behalf.
-        if (len(User.objects.filter(username=sauthor)) == 0):
-            suser = User.objects.create_user(sauthor, 'socknet@socknet.com', str(uuid.uuid4))
-            suser.save()
+            # If it's a new SockNet poster, make a fake user on their behalf.
+            if (len(User.objects.filter(username=sauthor)) == 0):
+                suser = User.objects.create_user(sauthor, 'socknet@socknet.com', str(uuid.uuid4))
+                suser.save()
 
-        sockPost = models.Post(author=User.objects.filter(username=sauthor)[0], text=stext, title=stitle, id=sid, image='sock.png', published_date=timezone.now(), source="SockNet", host="SockNet")
-        if (len(Post.objects.filter(id=sid)) == 0):
-            sockPost.save()
+            sockPost = models.Post(author=User.objects.filter(username=sauthor)[0], text=stext, title=stitle, id=sid, image='sock.png', published_date=timezone.now(), source="SockNet", host="SockNet")
+            if (len(Post.objects.filter(id=sid)) == 0):
+                sockPost.save()
 
-        # Now we handle comments!
-        if (len(i['comments']) > 0):
-            for j in i['comments']:
-                cid = j['id']
-                ctext = j['comment']
-                cauthor = j['author']['displayName']
-                # If it's a new SockNet poster, make a fake user on their behalf.
-                if (len(User.objects.filter(username=cauthor)) == 0):
-                    cuser = User.objects.create_user(cauthor, 'socknet@socknet.com', str(uuid.uuid4))
-                    cuser.save()
-                else:
-                    cuser = User.objects.filter(username=cauthor)[0]
+            # Now we handle comments!
+            if (len(i['comments']) > 0):
+                for j in i['comments']:
+                    cid = j['id']
+                    ctext = j['comment']
+                    cauthor = j['author']['displayName']
+                    # If it's a new SockNet poster, make a fake user on their behalf.
+                    if (len(User.objects.filter(username=cauthor)) == 0):
+                        cuser = User.objects.create_user(cauthor, 'socknet@socknet.com', str(uuid.uuid4))
+                        cuser.save()
+                    else:
+                        cuser = User.objects.filter(username=cauthor)[0]
 
-                sockComm = models.Comment(post=sockPost, author=cuser, text=ctext, id=cid, created_date=timezone.now())
-                if (len(Comment.objects.filter(id=cid)) == 0):
-                    sockComm.save()
+                    sockComm = models.Comment(post=sockPost, author=cuser, text=ctext, id=cid, created_date=timezone.now())
+                    if (len(Comment.objects.filter(id=cid)) == 0):
+                        sockComm.save()
         
     
     posts = Post.objects.filter(published_date__lte=timezone.now())
