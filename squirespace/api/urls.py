add4576558ad8bc9dc41c138bff0031786151e0e
Daniel <dsopel@ualberta.ca>
2016-11-23 20:29:34 -0700

    bad regex in url broke stuff

--- squirespace/api/urls.py
+++ squirespace/api/urls.py
@@ -5,15 +5,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^author/(?P<pk>[\w-]+)/posts', views.UserPosts.as_view()),
+    url('^author/(?P<pk>[0-9A-Fa-f-]+)/posts', views.UserPosts.as_view()),
     url('^author/$', views.UserList.as_view()),
     url('^author/posts', views.UserViewablePosts.as_view()),
-    url('^author/(?P<pk>[\w-]+)', views.UserDetail.as_view()),
-    url('^posts/(?P<pk>[\w-]+)/comments', views.PostDetailComments.as_view()),
-    url('^posts/(?P<pk>[\w-]+)', views.PostDetail.as_view()),
+    url('^author/(?P<pk>[0-9A-Fa-f-]+)', views.UserDetail.as_view()),
+    url('^posts/(?P<pk>[0-9A-Fa-f-]+)/comments', views.PostDetailComments.as_view()),
+    url('^posts/(?P<pk>[0-9A-Fa-f-]+)', views.PostDetail.as_view()),
     url('^posts/$', views.PostList.as_view()),
     url('^author/posts', views.UserViewablePosts.as_view()),
-    url('^friends/(?P<pk>[\w-]+)/$', views.UsersFriends.as_view()),
+    url('^friends/(?P<pk>[0-9A-Fa-f-]+)/$', views.UsersFriends.as_view()),
     url('^friends/(?P<pk1>[0-9A-Fa-f-]+)/(?P<pk2>[0-9A-Fa-f-]+)/$', views.AreTheseTwoUsersFriends.as_view()),
 ] 
 
